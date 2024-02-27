fb9b69943854bf1fa969de4f7b1d190cfbc441a0
白告月当空照 <1932824470@qq.com>
2024-02-27 20:06:54 +0800

    test

---
+++ CreateMatrix.cpp
@@ -0,0 +1,7 @@
+/*
+    创建迷宫矩阵
+    1.用户输入矩阵的长m、宽n；
+    2.用户输入障碍的最小长度obsMinL、最大长度obsMaxL、障碍的个数obsNum；
+    3.生成初始矩阵initMatrix；
+    4.生成obsNum个障碍的起始x、y位置以及横数方向obsDir；
+*/
---
+++ head.hpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <thread>
+#include <ctime>
+
+using namespace std;
